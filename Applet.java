d4e2c54341cb75cb5f135df092f62bc89f782c9f
London <london.brunell@gmail.com>
2017-04-02 12:17:17 -0400

    Create Applet.java
    This is a base applet file, with all methods to be implemented already present.

---
+++ Applet.java
@@ -0,0 +1,51 @@
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JApplet;
+
+/**
+ *
+ * @author London, Javier, Jose, Rachel, Rob
+ */
+public class RollTheBall extends JApplet implements MouseListener{
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    @Override
+    public void init() {
+        //  creation of things like buttonms goes here.
+
+    }
+
+    /**
+     * Draws the applet so that graphics appear.
+     * 
+     * @param g, the graphics object used to paint.
+     */
+    @Override
+    public void paint(Graphics g){
+        
+        
+    }
+    
+    // TODO overwrite start(), stop() and destroy() methods
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //TODO Add code here
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
